09e376e1d6e3584b4128523bae29b01855fc8cbd
KotlyarovSergey <zews-sv@rambler.ru>
2024-05-08 13:45:58 +0300

    before change with use fragments

--- app/src/main/java/com/ksv/workoutschedule/WorkoutPlan.kt
+++ app/src/main/java/com/ksv/workoutschedule/WorkoutPlan.kt
@@ -1,15 +1,7 @@
 package com.ksv.workoutschedule
 
-val pressPlan1 =
-    listOf<String>("Маятник", "Ситап", "Косые скручивания", "Книжка", "Скручивания с поворотом")
-val pressPlan2 = listOf<String>("Сотня", "Скамья", "Велосипед", "Планка", "Твист")
-val pressPlan3 = listOf<String>("Маятник", "Косые скручивания", "Скамья", "Планка", "Сотня")
-
-const val barPlan1 = "Подтягивания широким хватом"
-const val barPlan2 = "Подтягивания нормальным хватом"
-
 class WorkoutPlan(pressPlan: PressPlan, barExercise: BarPlan) {
-    var exercises: List<String> = listOf()
+    var exercises = listOf<String>()
         private set
 
     var press = pressPlan
@@ -18,18 +10,9 @@ class WorkoutPlan(pressPlan: PressPlan, barExercise: BarPlan) {
         private set
 
     init {
-        generateCurrentExercise()
+        makeListOfExercise()
     }
 
-    companion object Plans {
-        enum class PressPlan {
-            FIRST, SECOND, THIRST
-        }
-
-        enum class BarPlan {
-            FIRST, SECOND
-        }
-    }
 
     fun next(): List<String> {
         press = when (press) {
@@ -37,14 +20,11 @@ class WorkoutPlan(pressPlan: PressPlan, barExercise: BarPlan) {
             PressPlan.SECOND -> PressPlan.THIRST
             PressPlan.THIRST -> PressPlan.FIRST
         }
-
         bar = when (bar) {
             BarPlan.FIRST -> BarPlan.SECOND
             BarPlan.SECOND -> BarPlan.FIRST
         }
-
-        generateCurrentExercise()
-
+        makeListOfExercise()
         return exercises
     }
 
@@ -54,14 +34,11 @@ class WorkoutPlan(pressPlan: PressPlan, barExercise: BarPlan) {
             PressPlan.SECOND -> PressPlan.FIRST
             PressPlan.THIRST -> PressPlan.SECOND
         }
-
         bar = when (bar) {
             BarPlan.FIRST -> BarPlan.SECOND
             BarPlan.SECOND -> BarPlan.FIRST
         }
-
-        generateCurrentExercise()
-
+        makeListOfExercise()
         return exercises
     }
 
@@ -70,18 +47,35 @@ class WorkoutPlan(pressPlan: PressPlan, barExercise: BarPlan) {
         return ""
     }
 
-    private fun generateCurrentExercise() {
+    private fun makeListOfExercise() {
         val exerciseList = when (press) {
             PressPlan.FIRST -> pressPlan1.toMutableList()
             PressPlan.SECOND -> pressPlan2.toMutableList()
             PressPlan.THIRST -> pressPlan3.toMutableList()
         }
-
         when (bar) {
             BarPlan.FIRST -> exerciseList.add(barPlan1)
             BarPlan.SECOND -> exerciseList.add(barPlan2)
         }
-
         exercises = exerciseList.toList()
     }
+
+    companion object Plans {
+        enum class PressPlan {
+            FIRST, SECOND, THIRST
+        }
+        enum class BarPlan {
+            FIRST, SECOND
+        }
+
+        val pressPlan1 =
+            listOf("Маятник", "Ситап", "Косые скручивания", "Книжка", "Скручивания с поворотом")
+        val pressPlan2 = listOf("Сотня", "Скамья", "Велосипед", "Планка", "Твист")
+        val pressPlan3 = listOf("Маятник", "Косые скручивания", "Скамья", "Планка", "Сотня")
+
+        const val barPlan1 = "Подтягивания широким хватом"
+        const val barPlan2 = "Подтягивания нормальным хватом"
+    }
+
+
 }
