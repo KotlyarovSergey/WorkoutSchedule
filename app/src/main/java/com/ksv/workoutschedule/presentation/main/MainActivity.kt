e5dfac35f96ec3ebb31ef5abf4320e1328408a0d
KotlyarovSergey <zews-sv@rambler.ru>
2024-07-29 20:00:37 +0300

    Remove Setting menu item. Hide History button on HistoryFragment.

--- app/src/main/java/com/ksv/workoutschedule/presentation/main/MainActivity.kt
+++ app/src/main/java/com/ksv/workoutschedule/presentation/main/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.view.iterator
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.ksv.workoutschedule.R
@@ -12,71 +13,51 @@ import com.ksv.workoutschedule.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var mMenu: Menu? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        hideMenuItem(menu)
 
         supportFragmentManager.addOnBackStackChangedListener {
-            Log.d("ksvlog", "OnBackStackChangedListener")
+            hideMenuItem(menu)
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
 
-            var fragment = supportFragmentManager.findFragmentByTag("HISTORY_FRAG")
-            if (fragment != null) {
-                if (fragment.isVisible) {
-//                        Toast.makeText(this@MainActivity, "HISTORY_FRAG", Toast.LENGTH_LONG).show()
-                    Log.d("ksvlog", "HISTORY_FRAG")
-                }
+    private fun hideMenuItem(menu: Menu?) {
+        // visible all items
+        val iterator = menu?.iterator()
+        if (iterator != null) {
+            for (item in iterator) {
+                item.isVisible = true
             }
-
-            fragment = supportFragmentManager.findFragmentByTag("SETTING_FRAG")
-            if (fragment != null) {
-                if (fragment.isVisible) {
-//                        Toast.makeText(this@MainActivity, "SETTING_FRAG", Toast.LENGTH_LONG).show()
-                    Log.d("ksvlog", "SETTING_FRAG")
-                }
+        }
+        // get current fragment and hide respective item
+        val frag = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        when (frag) {
+            is HistoryFragment -> {
+                menu?.findItem(R.id.menu_history)?.isVisible = false
             }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        mMenu = menu
-
-        menuInflater.inflate(R.menu.menu_main, menu)
-//        val menuItem = menu?.findItem(R.id.menu_history)
-//        menuItem?.isVisible = false
-        return true
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            // TODO сделать нормально через Навигацию
-            R.id.menu_settings -> {
-//                mMenu?.findItem(R.id.menu_history)?.isVisible = true
-                val fr = supportFragmentManager.findFragmentById(R.id.fragment_container)
-                if (fr !is SettingsFragment) {
-                    supportFragmentManager.commit {
-                        replace<SettingsFragment>(binding.fragmentContainer.id, "SETTING_FRAG")
-                        addToBackStack(SettingsFragment::javaClass.name)
-                    }
-                }
-                true
-            }
-
             R.id.menu_history -> {
-//                supportFragmentManager.popBackStack()
-                val fr = supportFragmentManager.findFragmentById(R.id.fragment_container)
-                if (fr !is HistoryFragment) {
+                val fragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+                if (fragment !is HistoryFragment) {
                     supportFragmentManager.commit {
-                        replace<HistoryFragment>(binding.fragmentContainer.id, "HISTORY_FRAG")
+                        replace<HistoryFragment>(binding.fragmentContainer.id, HistoryFragment::class.java.name)
                         addToBackStack(HistoryFragment::javaClass.name)
                     }
                 }
-                item.isVisible = false
-//                mMenu?.findItem(R.id.menu_history)?.isVisible = false
-//                Log.d("ksvlog", "${supportFragmentManager.backStackEntryCount}")
                 true
             }
 
